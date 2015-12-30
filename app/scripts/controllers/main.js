bbab3570da7c72f77cb8c2b3f0346bf2c91e6325
Bob Webb <bob.wbb@googlemail.com>
2015-12-30 20:54:24 +0000

    can now add items to the basket, as per part 3 of the assignment

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,30 @@
  * Controller of the jstestApp
  */
 angular.module('jstestApp')
-  .controller('MainCtrl', ['$scope', 'MenuService', function ($scope, MenuService) {
+    .controller('MainCtrl', ['$scope', 'MenuService', function ($scope, MenuService) {
 	$scope.menu = {};
-    MenuService.get('/data/menu.json').success(function(data) {
-	  $scope.menu = data;
+	var menu_lookup = {}
+	MenuService.get('/data/menu.json').success(function(data) {
+	    $scope.menu = data;
 	});
 
-  }
+	$scope.basket = [];
+
+	$scope.total_price = 0;
+
+	$scope.add_to_basket = function(item_id){
+	    var meals = $scope.menu.meals;
+
+	    var item;
+	    for(var i=0; i < meals.length; i++){
+		if(meals[i].id == item_id){
+		    item = meals[i];
+		    break;
+		}
+	    }
+
+	    $scope.basket.push(item);
+	    $scope.total_price += parseFloat(item.price);
+	};
+    }
 ]);
