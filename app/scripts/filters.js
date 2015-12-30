5880491c5023c794a6e803a43b70cd911840173e
Bob Webb <bob.wbb@googlemail.com>
2015-12-30 23:14:12 +0000

    added 'use strict' to filters.js

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('jstestFilters', []).filter('is_tag', function() {
     return function(input) {
 	var tags = ['charcoal', 'cheese', 'chicken', 'grilled', 'high-protein', 'lamb', 'pasta', 'peanut', 'pork', 'seafood', 'snack', 'spicy', 'starter', 'sweet', 'vegetarian'];
